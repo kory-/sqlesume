73139728e73daf7ce89eb14d8ae76f0833c626b2
kory <momo0907@gmail.com>
2024-11-29 15:11:33 +0900

    Update wrangler.toml

--- pages/api/resume-data.ts
+++ pages/api/resume-data.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import resumeData from 'data/resume_data.json';
 
 // Edge Runtimeを設定
-export const runtime = 'experimental-edge';
+export const runtime = 'edge';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(resumeData);
