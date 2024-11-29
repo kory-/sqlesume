7b10ea9c2f557c6331ceaa675ef58f6023e78ce7
kory <momo0907@gmail.com>
2024-11-29 15:35:40 +0900

    delete statics

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { Terminal } from 'components/Terminal';
 import resumeData from 'data/resume_data.json';
 
 // Edge Runtimeを設定
-export const runtime = 'experimental-edge';
+// export const runtime = 'experimental-edge';
 
 // SSRでデータを取得
 export const getServerSideProps: GetServerSideProps = async () => {
