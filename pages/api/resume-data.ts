67bb9205e421920ee3483361d2fa170ea67a83a3
kory <momo0907@gmail.com>
2024-11-29 15:54:12 +0900

    delete cf pages setting

--- pages/api/resume-data.ts
+++ pages/api/resume-data.ts
@@ -1,9 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import resumeData from 'data/resume_data.json';
 
-// Edge Runtimeを設定
-// export const runtime = 'edge';
-
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.status(200).json(resumeData);
 } 
