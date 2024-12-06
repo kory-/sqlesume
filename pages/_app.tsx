27c75f5c061472daac559d271521454deb685291
kory <momo0907@gmail.com>
2024-12-06 13:56:52 +0900

    vercel/analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 import '../styles/console.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className="bg-terminal-bg min-h-screen">
       <Component {...pageProps} />
+      <Analytics />
     </div>
   )
 } 
